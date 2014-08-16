be4a54edeb766fc6698d5781265225c446a114be
Alexandre-Georges <alexandre.georges.ra@gmail.com>
2014-08-16 14:15:01 -0700

    Displaying agents

--- web/model/agent.dart
+++ web/model/agent.dart
@@ -11,7 +11,10 @@ class Agent extends Renderable {
   int _xIndex;
   int _yIndex;
   
+  Agent(this._xIndex, this._yIndex, this._model);
+  
   void render(CanvasElement canvas, CanvasRenderingContext2D context) {
-    context.fillRect(_xIndex, _yIndex, canvas.width, canvas.height);
+    context.setFillColorRgb(155, 80, 80);
+    context.fillRect(_xIndex * _model.unitWidth, _yIndex * _model.unitHeight, _model.unitWidth, _model.unitHeight);
   }
 }
--- web/model/model.dart
+++ web/model/model.dart
@@ -2,6 +2,7 @@ library model;
 
 import 'dart:html';
 import '../rendering/renderable.dart';
+import '../utils/array2D.dart';
 import 'agent.dart';
 
 class Model extends Renderable {
@@ -12,15 +13,23 @@ class Model extends Renderable {
   int _unitWidth;
   int _unitHeight;
   
-  List<List<Agent>> agents;
+  Array2D<Agent> agents;
   
   Model() {
-    agents = new List();
+    agents = new Array2D(this._width, this._height);
+    agents.add(1, 0, new Agent(1, 0, this));
   }
   
   void render(CanvasElement canvas, CanvasRenderingContext2D context) {
+    context.setFillColorRgb(0, 0, 0);
     this._unitWidth = canvas.width ~/ _width;
     this._unitHeight = canvas.height ~/ _height;
     context.fillRect(0, 0, this._unitWidth * _width, this._unitHeight * _height);
+    agents.toList().forEach((Agent agent) {
+      agent.render(canvas, context);
+    });
   }
+
+  int get unitWidth => this._unitWidth;
+  int get unitHeight => this._unitHeight;
 }
