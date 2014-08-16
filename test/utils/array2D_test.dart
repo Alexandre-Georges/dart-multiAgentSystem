6a41d94dfba879220eb5b18554469abf538f86ab
Alexandre-Georges <alexandre.georges.ra@gmail.com>
2014-08-16 13:44:28 -0700

    Updated tests for array2D

--- test/utils/array2D_test.dart
+++ test/utils/array2D_test.dart
@@ -2,87 +2,129 @@ import 'package:unittest/unittest.dart';
 import '../../web/utils/array2D.dart';
 
 main() {
+//Some third party entries in your sources.list were disabled. You can re-enable them after the upgrade with the 'software-properties' tool or your package manager.
+  
+  group('getIndex', () {
+    
+    Array2D<String> array2D;
+    
+    setUp(() {
+      array2D = new Array2D<String>(3, 2);
+    });
+  
+    test("incorrect x", () {
+      expect(() {
+        array2D.getIndex(-1, 0);
+      }, throwsArgumentError);
+      expect(() {
+        array2D.getIndex(3, 0);
+      }, throwsArgumentError);
+    });
+  
+    test("incorrect y", () {
+      expect(() {
+        array2D.getIndex(0, -1);
+      }, throwsArgumentError);
+      expect(() {
+        array2D.getIndex(0, 2);
+      }, throwsArgumentError);
+    });
+    
+    test("getIndex", () {
+      expect(array2D.getIndex(0, 0), 0);
+      expect(array2D.getIndex(0, 1), 1);
+      expect(array2D.getIndex(1, 0), 2);
+      expect(array2D.getIndex(1, 1), 3);
+      expect(array2D.getIndex(2, 0), 4);
+      expect(array2D.getIndex(2, 1), 5);
+    });
 
-  Array2D<String> array2D = new Array2D<String>(3, 2);
-  
-  test("negative x", () {
-    expect(() {
-      array2D.getIndex(-1, 0);
-    }, throwsArgumentError);
-  });
-
-  test("negative y", () {
-    expect(() {
-      array2D.getIndex(0, -1);
-    }, throwsArgumentError);
-  });
-
-  test("x is too high", () {
-    expect(() {
-      array2D.getIndex(3, 0);
-    }, throwsArgumentError);
-  });
-
-  test("y is too high", () {
-    expect(() {
-      array2D.getIndex(0, 3);
-    }, throwsArgumentError);
-  });
-  
-  test("index (0, 0)", () {
-    expect(array2D.getIndex(0, 0), 0);
-  });
-  
-  test("index (0, 1)", () {
-    expect(array2D.getIndex(0, 1), 1);
-  });
-  
-  test("index (1, 0)", () {
-    expect(array2D.getIndex(1, 0), 2);
-  });
-  
-  test("index (1, 1)", () {
-    expect(array2D.getIndex(1, 1), 3);
   });
   
-  test("index (2, 0)", () {
-    expect(array2D.getIndex(2, 0), 4);
+  group('add', () {
+    
+    Array2D<String> array2D;
+    
+    setUp(() {
+      array2D = new Array2D<String>(3, 2);
+    });
+    
+    test("incorrect x", () {
+      expect(() {
+        array2D.add(-1, 0, "val -1.0");
+      }, throwsArgumentError);
+      expect(() {
+        array2D.add(3, 0, "val 3.0");
+      }, throwsArgumentError);
+    });
+  
+    test("incorrect y", () {
+      expect(() {
+        array2D.add(0, -1, "val 0.-1");
+      }, throwsArgumentError);
+      expect(() {
+        array2D.add(0, 2, "val 0.2");
+      }, throwsArgumentError);
+    });
+
+    test("add", () {
+      expect(() {
+        array2D.add(0, 0, "val 0.0");
+        array2D.add(0, 1, "val 0.1");
+        array2D.add(1, 0, "val 1.0");
+        array2D.add(1, 1, "val 1.1");
+        array2D.add(2, 0, "val 2.0");
+        array2D.add(2, 1, "val 2.1");
+      }, returnsNormally);
+    });
   });
   
-  test("index (2, 1)", () {
-    expect(array2D.getIndex(2, 1), 5);
-  });
+  group('get', () {
+    
+    Array2D<String> array2D;
+    
+    setUp(() {
+      array2D = new Array2D<String>(3, 2);
+      array2D.add(0, 0, "val 0.0");
+      array2D.add(0, 1, "val 0.1");
+      array2D.add(1, 0, "val 1.0");
+      array2D.add(1, 1, "val 1.1");
+      array2D.add(2, 0, "val 2.0");
+      array2D.add(2, 1, "val 2.1");
+    });
+    
+    test("incorrect x", () {
+      expect(() {
+        array2D.get(-1, 0);
+      }, throwsArgumentError);
+      expect(() {
+        array2D.get(3, 0);
+      }, throwsArgumentError);
+    });
   
-  array2D.add(0, 0, "val 0.0");
-  array2D.add(0, 1, "val 0.1");
-  array2D.add(1, 0, "val 1.0");
-  array2D.add(1, 1, "val 1.1");
-  array2D.add(2, 0, "val 2.0");
-  array2D.add(2, 1, "val 2.1");
-
-  test("get (0, 0)", () {
-    expect(array2D.get(0, 0), "val 0.0");
+    test("incorrect y", () {
+      expect(() {
+        array2D.get(0, -1);
+      }, throwsArgumentError);
+      expect(() {
+        array2D.get(0, 2);
+      }, throwsArgumentError);
+    });
+    
+    test("get", () {
+      expect(array2D.get(0, 0), new isInstanceOf<String>());
+      expect(array2D.get(0, 1), new isInstanceOf<String>());
+      expect(array2D.get(1, 0), new isInstanceOf<String>());
+      expect(array2D.get(1, 1), new isInstanceOf<String>());
+      expect(array2D.get(2, 0), new isInstanceOf<String>());
+      expect(array2D.get(2, 1), new isInstanceOf<String>());
+      expect(array2D.get(0, 0), "val 0.0");
+      expect(array2D.get(0, 1), "val 0.1");
+      expect(array2D.get(1, 0), "val 1.0");
+      expect(array2D.get(1, 1), "val 1.1");
+      expect(array2D.get(2, 0), "val 2.0");
+      expect(array2D.get(2, 1), "val 2.1");
+    });
   });
-
-  test("get (0, 1)", () {
-    expect(array2D.get(0, 1), "val 0.1");
-  });
-
-  test("get (1, 0)", () {
-    expect(array2D.get(1, 0), "val 1.0");
-  });
-
-  test("get (1, 1)", () {
-    expect(array2D.get(1, 1), "val 1.1");
-  });
-
-  test("get (2, 0)", () {
-    expect(array2D.get(2, 0), "val 2.0");
-  });
-
-  test("get (2, 1)", () {
-    expect(array2D.get(2, 1), "val 2.1");
-  });
-
   
 }
