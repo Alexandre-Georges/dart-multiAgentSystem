5296b916069b3e09084637de2e9270fd2d5603ee
Alexandre-Georges <alexandre.georges.ra@gmail.com>
2014-08-19 20:57:45 -0700

    Random initialization

--- web/main.dart
+++ web/main.dart
@@ -3,14 +3,14 @@ import 'rendering/renderer.dart';
 import 'model/implementation/gameOfLife/model.dart';
 
 void main() {
-  GameOfLifeModel model = new GameOfLifeModel(4, 3);
+  GameOfLifeModel model = new GameOfLifeModel(50, 60);
   Renderer renderer = new Renderer(model);
   renderer.start();
-  iterate(model);
+  //iterate(model);
 }
 
 void iterate(GameOfLifeModel model) {
-  new Timer(new Duration(seconds: 2), () {
+  new Timer(new Duration(milliseconds: 200), () {
     model.computeNextStep();
     model.evolve();
     iterate(model);
--- web/model/implementation/gameOfLife/model.dart
+++ web/model/implementation/gameOfLife/model.dart
@@ -1,21 +1,32 @@
 library model;
 
+import 'dart:math';
 import '../../model.dart';
 import 'cell.dart';
 
 class GameOfLifeModel extends Model {
   GameOfLifeModel(int width, int height) : super(width, height) {
-    this.cells.add(new GameOfLifeCell(false, 0, 0, this));
-    this.cells.add(new GameOfLifeCell(false, 0, 1, this));
-    this.cells.add(new GameOfLifeCell(false, 0, 2, this));
-    this.cells.add(new GameOfLifeCell(true, 1, 0, this));
-    this.cells.add(new GameOfLifeCell(false, 1, 1, this));
-    this.cells.add(new GameOfLifeCell(true, 1, 2, this));
-    this.cells.add(new GameOfLifeCell(false, 2, 0, this));
-    this.cells.add(new GameOfLifeCell(true, 2, 1, this));
-    this.cells.add(new GameOfLifeCell(false, 2, 2, this));
-    this.cells.add(new GameOfLifeCell(false, 3, 0, this));
-    this.cells.add(new GameOfLifeCell(true, 3, 1, this));
-    this.cells.add(new GameOfLifeCell(false, 3, 2, this));
+    int aliveNumber = 20;
+    int remainingCells = width * height;
+    for (int x = 0; x < width; x++) {
+      for (int y = 0; y < height; y++) {
+        bool isAlive = isAliveRandomInitialization(aliveNumber, remainingCells);
+        if (isAlive) {
+          aliveNumber--;
+        }
+        remainingCells--;
+        this.cells.add(new GameOfLifeCell(isAlive, x, y, this));
+      }
+    }
+  }
+  
+  bool isAliveRandomInitialization(int aliveNumber, int remainingCells) {
+    Random random = new Random();
+    double probability = random.nextDouble();
+    
+    if (probability > aliveNumber / remainingCells) {
+      return false;
+    }
+    return true;
   }
 }
