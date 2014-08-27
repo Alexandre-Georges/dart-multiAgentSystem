dd3e1bec483f5ee46b2b3b6840336d3a908a5dd5
Alexandre-Georges <alexandre.georges.ra@gmail.com>
2014-08-27 15:47:23 -0700

    Buttons and fields

--- web/main.dart
+++ web/main.dart
@@ -1,11 +1,25 @@
 import 'dart:async';
 import 'dart:html';
 import 'rendering/renderer.dart';
+import 'model/model.dart';
 import 'model/implementation/gameOfLife/model.dart';
 
+bool started = false;
+Model model = null;
+
 void main() {
+  
+  ButtonInputElement start = querySelector('#startButton');
+  start.addEventListener("click", (MouseEvent mouseEvent){
+    started = !started;
+    if (started) {
+      iterate(model);
+    }
+    start.value = started ? 'Stop' : 'Start';
+  });
+  
   CanvasElement canvasElement = querySelector('#canvas');
-  GameOfLifeModel model = new GameOfLifeModel(50, 60);
+  model = new GameOfLifeModel(50, 60);
   model.addListeners(canvasElement);
   Renderer renderer = new Renderer(model, canvasElement);
   renderer.start();
@@ -13,10 +27,12 @@ void main() {
 }
 
 void iterate(GameOfLifeModel model) {
-  new Timer(new Duration(milliseconds: 200), () {
+  if (started) {
     model.computeNextStep();
     model.evolve();
-    iterate(model);
-  });
+    new Timer(new Duration(milliseconds: 2000), () {
+        iterate(model);
+    });
+  }
 }
 
